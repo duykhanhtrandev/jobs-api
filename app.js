90eca55673938ebc14cd3b665cae6b8a8d164692
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-05-17 17:17:43 +0700

    swagger ui

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 require("express-async-errors");
-const express = require("express");
-const app = express();
 
 // extra security packages
 const helmet = require("helmet");
@@ -9,14 +7,19 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
-// connectDB
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+const express = require("express");
+const app = express();
+
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
-
 // routers
 const authRouter = require("./routes/authRoutes");
 const jobsRouter = require("./routes/jobsRoutes");
-
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -32,7 +35,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// extra packages
+
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
